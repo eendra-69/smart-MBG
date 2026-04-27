4ffa547aa4be89a0d124cdf8b0a376c271b21b53
eendra-69 <indra.cahyadi@trunojoyo.ac.id>
2026-04-27 08:14:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ if st.button("🚀 Buat Jadwal Menu!", type="primary"):
 
             for k in ZAT_GIZI:
                 kandungan_terpilih = lpSum([df_menu[df_menu['nama_menu'] == i][k].values[0] * x[i][t] for i in menu_list])
-                model += kandungan_terpilih >= 0.4 * target[k] # Hard limit 40-80%
+                model += kandungan_terpilih >= 0.3 * target[k] # Hard limit 40-80%
                 model += kandungan_terpilih + shortage[k][t] >= 0.9 * target[k] # Soft limit 90-100%
                 
                 # Batasan Karbohidrat minimal 40 gram
